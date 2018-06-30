e6f45ef15ef0f5c12320a17c8de842183b260035
AdamG <36753247+AdamG@users.noreply.github.com>
2018-06-30 18:10:59 -0500

    Added to_fraction() function and fixed sign consistency issues

--- fraction.py
+++ fraction.py
@@ -2,12 +2,11 @@ class frac:
     def __init__(self, numer, denom=1):
         assert (isinstance(numer, int) and isinstance(denom, int))
 
-        self.numer = numer
-        if (denom != 0):
-            self.denom = denom
-        else:
+        if (denom == 0):
             raise ValueError("denominator cannot be 0.")
-
+        else:
+            self.numer = numer
+            self.denom = denom
 
     def __repr__(self):
         return f'frac({self.numer}, {self.denom})'
@@ -17,10 +16,21 @@ class frac:
         return f'{self.numer}/{self.denom}'
 
     # Auiliary Methods =========================================================
+    @staticmethod
+    def to_fraction(decimal):
+        """Converts decimal values into 'frac' objects."""
+        magnitude = 1
+        while (decimal // 1) != decimal:
+            decimal *= 10
+            magnitude *= 10
+        return frac(int(decimal), magnitude).simplify()
 
     @staticmethod
-    def _gcd(d1, d2):
-        """Euclidean algorithm for finding gcd"""
+    def _gcd(n1, n2):
+        """Euclidean algorithm for finding greatest common divisor"""
+        d1 = abs(n1)
+        d2 = abs(n2)
+
         a = max(d1, d2)
         b = min(d1, d2)
         while True:
@@ -35,8 +45,9 @@ class frac:
 
 
     @staticmethod
-    def _lcm(d1, d2):
-        return (d1 // frac._gcd(d1, d2)) * d2
+    def _lcm(n1, n2):
+        """Returns the least common multiple of two integers."""
+        return (n1 // frac._gcd(n1, n2)) * n2
 
 
     def reciprocal(self):
@@ -50,10 +61,11 @@ class frac:
 
 
     def get_proper(self):
+        """Returns the fraction as a tuple containing an integer and a proper fraction."""
         coeff = self.numer // self.denom
         pfrac = self.numer % self.denom
 
-        return (coeff, frac(pfrac, self.denom).simplify())
+        return (coeff, frac(pfrac, abs(self.denom)).simplify())
 
 
     #Addition ==================================================================
@@ -66,7 +78,7 @@ class frac:
         elif isinstance(other, int):
             n2 = other * self.denom
         else:
-            raise TypeError("Second operand must be frac() object or int.")
+            raise TypeError("Second operand must be 'frac' object or int.")
 
         n1 = (d // self.denom) * self.numer
 
@@ -175,6 +187,3 @@ class frac:
 
     def __bool__(self):
         return self.numer != 0
-
-a = frac(30,8)
-print(a.get_proper())
