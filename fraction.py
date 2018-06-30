cc3df9894f6c0ac926ab7360f1afa4cd29e20d79
AdamG <36753247+AdamG@users.noreply.github.com>
2018-06-30 15:40:47 -0500

    finished adding support for comparison with integers

--- fraction.py
+++ fraction.py
@@ -149,13 +149,19 @@ class frac:
         return not (self == other)
 
     def __lt__(self, other):
-        return (self.numer * other.denom) < (self.denom * other.numer)
+        if isinstance(other, frac):
+            return (self.numer * other.denom) < (self.denom * other.numer)
+        elif isinstance(other, int):
+            return (self.numer // self.denom) < other
 
     def __le__(self, other):
         return self < other or self == other
 
     def __gt__(self, other):
-        return (self.numer * other.denom) > (self.denom * other.numer)
+        if isinstance(other, frac):
+            return (self.numer * other.denom) > (self.denom * other.numer)
+        elif isinstance(other, int):
+            return (self.numer // self.denom) > other
 
     def __ge__(self, other):
         return self > other or self == other
